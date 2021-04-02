1cdddabf95f98d3f947a453b2f04c153a7dbf25d
JYOTIPRAKASH NAYAK <jpnayakchintu@gmail.com>
2021-04-02 13:30:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, render_template
 import pickle
-import cv2
 
 app = Flask(__name__)
 
-#model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -14,39 +13,6 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    # Load the cascade
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# To capture video from webcam.
-    cap = cv2.VideoCapture(0)
-# To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
-
-    while True:
-    # Read the frame
-        _, img = cap.read()
-
-    # Convert to grayscale
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Detect the faces
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    # Draw the rectangle around each face
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-    # Display
-        cv2.imshow('img', img)
-
-    # Stop if escape key is pressed
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
-
-# Release the VideoCapture object
-    cap.release()
-    """
     input_features = [float(x) for x in request.form.values()]
     features_value = [np.array(input_features)]
     
@@ -69,7 +35,7 @@ def predict():
         res_val = "no breast cancer"
         
 
-    return render_template('index.html', prediction_text='Patient has {}'.format(res_val))"""
+    return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
 
 if __name__ == "__main__":
     app.run()
