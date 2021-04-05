58e7220755bcf22090a8b17f5d91851ead888451
JYOTIPRAKASH NAYAK <jpnayakchintu@gmail.com>
2021-04-05 15:09:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,324 @@
 import numpy as np
 import pandas as pd
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import pickle
+import cv2
+import os
+from os import listdir
+from os.path import isfile, join
+import face_recognition as fr
+import face_recognition
+from time import sleep
+
+
+
+
+
+
+
 
 app = Flask(__name__)
-
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+
+
+
+@app.route('/upload_img',methods=['POST'])
+def upload_img():
+
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    def face_extractor(img):
+
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray,1.3,5)
+
+        if faces is():
+            return None
+
+        for(x,y,w,h) in faces:
+            cropped_face = img[y:y+h, x:x+w]
+
+        return cropped_face
+
+
+    cap = cv2.VideoCapture(0)
+    count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if face_extractor(frame) is not None:
+            count+=1
+            face = cv2.resize(face_extractor(frame),(200,200))
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+            file_name_path = './img/'+str(count)+'.jpg'
+
+            cv2.imwrite(file_name_path,face)
+
+            cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.imshow('Face Cropper',face)
+        else:
+            print("Face not found")
+            pass
+
+        if cv2.waitKey(1)==13 or count==100:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    print('Samples Colletion Completed ')
+
+
+
+
+
+@app.route('/video_recog',methods=['POST'])
+def video_recog():
+
+    data_path = './img/'
+    onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
+
+    Training_Data, Labels = [], []
+
+    for i, files in enumerate(onlyfiles):
+        image_path = data_path + onlyfiles[i]
+        images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        Training_Data.append(np.asarray(images, dtype=np.uint8))
+        Labels.append(i)
+
+    Labels = np.asarray(Labels, dtype=np.int32)
+
+    model = cv2.face.LBPHFaceRecognizer_create()
+
+    model.train(np.asarray(Training_Data), np.asarray(Labels))
+
+    print("Dataset Model Training Complete!!!!!")
+
+    face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    def face_detector(img, size = 0.5):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray,1.3,5)
+
+        if faces is():
+            return img,[]
+
+        for(x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,0),2)
+            roi = img[y:y+h, x:x+w]
+            roi = cv2.resize(roi, (200,200))
+
+        return img,roi
+
+    cap = cv2.VideoCapture(0)
+    while True:
+
+        ret, frame = cap.read()
+
+        image, face = face_detector(frame)
+
+        try:
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+            result = model.predict(face)
+
+            if result[1] < 500:
+                confidence = int(100*(1-(result[1])/300))
+
+
+
+            if confidence > 82:
+                cv2.putText(image, request.form.get('name'), (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.imshow('Face Cropper', image)
+
+            else:
+                cv2.putText(image, "Unknown", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                cv2.imshow('Face Cropper', image)
+
+
+        except:
+            cv2.putText(image, "Face Not Found", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+            cv2.imshow('Face Cropper', image)
+            pass
+
+        if cv2.waitKey(1)==13:
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+@app.route('/img_recog',methods=['POST'])
+def img_recog():
+    
+    def get_encoded_faces():
+        """
+        looks through the faces folder and encodes all
+        the faces
+        
+        :return: dict of (name, image encoded)
+        """
+        encoded = {}
+
+        for dirpath, dnames, fnames in os.walk("./img"):
+            for f in fnames:
+                if f.endswith(".jpg") or f.endswith(".png"):
+                    face = fr.load_image_file("img/" + f)
+                    encoding = fr.face_encodings(face)[0]
+                    encoded[f.split(".")[0]] = encoding
+
+        return encoded
+
+
+    def unknown_image_encoded(img):
+        """
+        encode a face given the file name
+        """
+        face = fr.load_image_file("img/" + img)
+        encoding = fr.face_encodings(face)[0]
+
+        return encoding
+
+
+    def classify_face(im):
+        """
+        will find all of the faces in a given image and label
+        them if it knows what they are
+        
+        :param im: str of file path
+        :return: list of face names
+        """
+        faces = get_encoded_faces()
+        faces_encoded = list(faces.values())
+        known_face_names = list(faces.keys())
+
+        img = cv2.imread(im, 1)
+ 
+        face_locations = face_recognition.face_locations(img)
+        unknown_face_encodings = face_recognition.face_encodings(img, face_locations)
+
+        face_names = []
+        for face_encoding in unknown_face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(faces_encoded, face_encoding)
+            name = "Unknown"
+            
+            # use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(faces_encoded, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                
+                face_names.append(request.form.get('name'))
+                
+                for (top, right, bottom, left), name in zip(face_locations, face_names):
+                    # Draw a box around the face
+                    cv2.rectangle(img, (left-20, top-20), (right+20, bottom+20), (255, 0, 0), 2)
+                    
+                    # Draw a label with a name below the face
+                    cv2.rectangle(img, (left-20, bottom -15), (right+20, bottom+20), (255, 0, 0), cv2.FILLED)
+                    font = cv2.FONT_HERSHEY_DUPLEX
+                    cv2.putText(img, name, (left -20, bottom + 15), font, 1.0, (255, 255, 255), 2)
+
+
+        # Display the resulting image
+        while True:
+            cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+            cv2.imshow('image', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return face_names 
+            
+
+    print(classify_face(str(request.form.get('img_recog'))))
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 @app.route('/predict',methods=['POST'])
 def predict():
-    input_features = [float(x) for x in request.form.values()]
-    features_value = [np.array(input_features)]
-    
-    features_name = ['mean radius', 'mean texture', 'mean perimeter', 'mean area',
-       'mean smoothness', 'mean compactness', 'mean concavity',
-       'mean concave points', 'mean symmetry', 'mean fractal dimension',
-       'radius error', 'texture error', 'perimeter error', 'area error',
-       'smoothness error', 'compactness error', 'concavity error',
-       'concave points error', 'symmetry error', 'fractal dimension error',
-       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
-       'worst smoothness', 'worst compactness', 'worst concavity',
-       'worst concave points', 'worst symmetry', 'worst fractal dimension']
-    
-    df = pd.DataFrame(features_value, columns=features_name)
-    output = model.predict(df)
-        
-    if output == 0:
-        res_val = "breast cancer"
-    else:
-        res_val = "no breast cancer"
-        
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-    return render_template('index.html', prediction_text='Patient has {}'.format(res_val))
+    # To capture video from webcam.
+    cap = cv2.VideoCapture(0)
+    # To use a video file as input
 
+    while True:
+    # Read the frame
+        _, img = cap.read()
+
+    # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # Display
+        cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+    # Release the VideoCapture object
+    cap.release()
+    
+
+
+@app.route('/img_detect',methods=['POST'])
+def img_detect():
+    
+    f = request.files['image']  
+    f.save(f.filename)
+    
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # Read the input image
+    img = cv2.imread(str(f.filename))
+
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # Display the output
+    cv2.imshow('img', img)
+    cv2.waitKey()
+    
+    
+    
 if __name__ == "__main__":
     app.run()
